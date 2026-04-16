598f7665ea8bc6915d34466acd0d77e942f1cb97
KiraCmy <my.cui@outlook.com>
2026-04-15 21:12:57 -0400

    fix: 修复 filter 重置导致的构建错误

--- src/views/project/ProjectList.vue
+++ src/views/project/ProjectList.vue
@@ -26,7 +26,7 @@
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="()=>{}">搜索</el-button>
-          <el-button @click="filter={status:'',designer:'',keyword:''}">重置</el-button>
+          <el-button @click="filter.status='';filter.designer='';filter.keyword=''">重置</el-button>
         </el-form-item>
       </el-form>
     </el-card>
